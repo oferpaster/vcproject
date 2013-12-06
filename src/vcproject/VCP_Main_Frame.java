070d9fd3c0311f49912460815436d4e7a8d7814c
oferpaster <oferpaster@gmail.com>
2013-12-06 15:52:56 +0200

    -add listener method

--- src/vcproject/VCP_Main_Frame.java
+++ src/vcproject/VCP_Main_Frame.java
@@ -1,7 +1,11 @@
 package vcproject;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VCP_Main_Frame extends JFrame {
 	
@@ -23,8 +27,31 @@ public class VCP_Main_Frame extends JFrame {
 		this.setContentPane(getMainPanel());
 		getContentPane().setBackground(SystemColor.activeCaption);
 		this.setSize(800, 600);
+		listners();
 		
 		
+	}
+	
+	private void listners(){
+		getMainPanel().getBtnExit().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame frame = new JFrame();
+
+				int result = JOptionPane.showConfirmDialog(frame,
+						"Are you sure you want to exit the application?",
+						"Exit Application", JOptionPane.YES_NO_OPTION);
+
+				if (result == JOptionPane.YES_OPTION)
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				closeMainFrame();
+			
+			}
+		});
+	}
+	
+	private void closeMainFrame() {
+		this.setVisible(false);
+		this.dispose();
 	}
 
 	public Main_Panel getMainPanel() {
